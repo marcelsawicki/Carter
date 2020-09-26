f1d57d78bc279506247c49fb03fc1900fdfb1e27
Marcel Sawicki <msawicki@it-dev.pl>
2020-09-26 10:21:14 +0200

    EF Context

---
+++ WebShop/WebShop/Model/EFCOreWebShopContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShop.Model
+{
+    public class EFCOreWebShopContext : DbContext
+    {
+            public DbSet<Board> Boards { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreWebDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+    }
+}
