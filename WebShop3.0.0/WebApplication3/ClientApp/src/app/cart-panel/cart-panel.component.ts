f9ebdbe48d67fb5239551c41ae431a76a4b442fd
Marcel Sawicki <msawicki@it-dev.pl>
2021-02-03 07:29:19 +0100

    Auth + Db

---
+++ WebShop3.0.0/WebApplication3/ClientApp/src/app/cart-panel/cart-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './cart-panel.component.html',
+})
+export class CartPanelComponent {
+}
---
+++ WebShop3.0.0/WebApplication3/ClientApp/src/app/fruits/fruits.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'fruits-data',
+  templateUrl: './fruits.component.html'
+})
+export class FruitsComponent {
+  public boards: Boards[];
+  Router: Router
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, router: Router) {
+    this.Router = router;
+    http.get<Boards[]>(baseUrl + 'fruits').subscribe(result => {
+      this.boards = result;
+    }, error => {
+      console.error('przkierowanie' + error);
+      this.Router.navigate(['/login-panel'])
+    });
+  }
+  public buyProcess() {
+    console.log("Buying begin");
+    let cartCount = document.getElementById('lblCartCount');
+    let incrementNumber = parseInt(cartCount.innerText) + 1;
+    cartCount.innerText = incrementNumber.toString();
+  }
+}
+
+interface Boards {
+  id: number;
+  name: string;
+}
