5e15c6a146cb4bde819c5c4677a82e0e01dd678c
Marcel Sawicki <msawicki@it-dev.pl>
2021-02-20 11:27:03 +0100

    Dodano 'modal window', popraiono okno logowania.

--- WebShop3.0.0/WebApplication3/ClientApp/src/app/counter/counter.component.ts
+++ WebShop3.0.0/WebApplication3/ClientApp/src/app/counter/counter.component.ts
@@ -10,4 +10,16 @@ export class CounterComponent {
   public incrementCounter() {
     this.currentCount++;
   }
+  public closeModalWindow() {
+    let modalwindow1 = document.getElementById('modalwindow1');
+    modalwindow1.hidden = true;
+  }
+  public openModalWindow() {
+    let modalwindow1 = document.getElementById('modalwindow1');
+    modalwindow1.hidden = false;
+  }
+  public saveChangesModalWindow() {
+    let modalwindowmsg = document.getElementById('modalwindowmsg');
+    modalwindowmsg.innerText = 'Not saved - in development.';
+  }
 }
