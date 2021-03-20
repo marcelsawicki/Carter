1ef4bb353f28e12b6a297e44e22607da0cea8ae8
Marcel Sawicki <msawicki@it-dev.pl>
2021-03-20 16:21:03 +0100

    Update

--- WebShop3.0.0/WebApplication3/ClientApp/src/app/cart-panel/cart-panel.component.ts
+++ WebShop3.0.0/WebApplication3/ClientApp/src/app/cart-panel/cart-panel.component.ts
@@ -1,24 +1,65 @@
 import { Component } from '@angular/core';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-home',
   templateUrl: './cart-panel.component.html',
 })
 export class CartPanelComponent {
-  public mylist: string[];
+  public mylist: Articles[];
+  public articles: Articles[];
+  public totalZK: number;
+  public totalSS: number;
+  public totalP: number;
   constructor()
   {
     let mylist = [];
     for (let i = 0; i < sessionStorage.length; i++) {
       let key = sessionStorage.key(i);
       let value = sessionStorage.getItem(key);
-      var obj = JSON.parse(value);
-      //mylist.push(obj.boardName);
-      mylist.push(value);
+      let obj = JSON.parse(value);
+      let prettyArticle: Articles = obj as Articles;
+      mylist.push(prettyArticle);
       this.mylist = mylist;
       console.log(key, value);
-    }
+    }  
   }
+  public calculateTotalZK(): number {
+    let totalZZZ: number = 0;;
+    for (let i = 0; i < sessionStorage.length; i++) {
+      let key = sessionStorage.key(i);
+      let value = sessionStorage.getItem(key);
+      let obj = JSON.parse(value);
+      let prettyArticle: Articles = obj as Articles;
+      totalZZZ += prettyArticle.priceZK;
+    }
+    return totalZZZ;
+  }
+
+  public calculateTotalSS(): number {
+    let totalSSS: number=0;
+    for (let i = 0; i < sessionStorage.length; i++) {
+      let key = sessionStorage.key(i);
+      let value = sessionStorage.getItem(key);
+      let obj = JSON.parse(value);
+      let prettyArticle: Articles = obj as Articles;
+      totalSSS += prettyArticle.priceSS;
+    }
+    return totalSSS;
+  }
+
+  public calculateTotalP(): number {
+    let totalPPP: number=0;
+    for (let i = 0; i < sessionStorage.length; i++) {
+      let key = sessionStorage.key(i);
+      let value = sessionStorage.getItem(key);
+      let obj = JSON.parse(value);
+      let prettyArticle: Articles = obj as Articles;
+      totalPPP += prettyArticle.priceP;
+    }
+    return totalPPP;
+  }
+
   public clearCart() {
     sessionStorage.clear();
     this.mylist = [];
@@ -26,6 +67,9 @@ export class CartPanelComponent {
     let cartCount = document.getElementById('lblCartCount');
     let incrementNumber = 0;
     cartCount.innerText = incrementNumber.toString();
+    this.totalZK = 0;
+    this.totalSS = 0;
+    this.totalP = 0;
   }
   public orderCart() {
     console.log("Making order...")
@@ -47,5 +91,17 @@ export class CartPanelComponent {
     // Called after the constructor and called  after the first ngOnChanges()
     let modalwindow1 = document.getElementById('modalwindow1');
     modalwindow1.hidden = true;
+    this.totalZK = this.calculateTotalZK();
+    this.totalSS = this.calculateTotalSS();
+    this.totalP = this.calculateTotalP();
   }
+}
+
+interface Articles {
+  id: number;
+  price: number;
+  priceZK: number;
+  priceSS: number;
+  priceP: number;
+  name: string;
 }
