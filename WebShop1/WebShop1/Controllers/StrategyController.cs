6ac81c8275e994c546f249f255f513ebc3f53824
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-11-20 15:28:46 +0100

    Strategy

---
+++ WebShop1/WebShop1/Controllers/StrategyController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebShop1.Domain;
+
+namespace WebShop1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StrategyController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            ISortingStrategy sortingStrategy = null;
+ 
+            List<string> voivodeshipResidence = new List<string> { "ab", "ac", "xa", "td" };
+            sortingStrategy = GetSortingOption(ObjectToSort.Surname);
+            sortingStrategy.Sort(voivodeshipResidence);
+
+            List<int> studentNumbers = new List<int> { 123456, 9876543, 345432 };
+            sortingStrategy = GetSortingOption(ObjectToSort.Book);
+            sortingStrategy.Sort(studentNumbers);
+
+            List<string> cityCartNumbers = new List<string> { "A123456", "B9876543","C345432" };
+            sortingStrategy = GetSortingOption(ObjectToSort.CityCard);
+            sortingStrategy.Sort(cityCartNumbers);
+            return null;
+        }
+
+        private static ISortingStrategy GetSortingOption(ObjectToSort objectToSort)
+        {
+            ISortingStrategy sortingStrategy = null;
+            switch (objectToSort)
+            {
+                case ObjectToSort.Book:
+                    sortingStrategy = new MergeSort();
+                    break;
+                case ObjectToSort.CityCard:
+                    sortingStrategy = new HeapSort();
+                    break;
+                case ObjectToSort.Surname:
+                    sortingStrategy = new QuickSort();
+                    break;
+                default:
+                    break;
+            }
+            return sortingStrategy;
+        }
+    }
+}
---
+++ WebShop1/WebShop1/Domain/HeapSort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop1.Domain
+{
+    public class HeapSort : ISortingStrategy
+    {
+        public void Sort<T>(List<T> dataToSort)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
