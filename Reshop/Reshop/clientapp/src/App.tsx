1f348cd65126d05feeead4d0bc9702aa3bb0eb18
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-05-27 12:10:26 +0200

    TasksList

--- Reshop/Reshop/clientapp/src/App.tsx
+++ Reshop/Reshop/clientapp/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import Message from './components/Message';
+import TasksList from './components/TasksList';
 
 function App() {
   return (
     <div className="App">
           <NavBar />
-          <Message />
+          <TasksList />
     </div>
   );
 }
